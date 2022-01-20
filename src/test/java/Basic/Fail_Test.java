210099f58f64cbf2d6f7fe31d19b4fbdae59c4a6
Chethan K M <Chethan.km@Triconinfotech.com>
2022-01-20 19:30:47 +0530

    Add one more testcase

---
+++ src/test/java/Basic/Fail_Test.java
@@ -0,0 +1,25 @@
+package Basic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Fail_Test {
+	
+	@Test
+	public void fail() {
+		 WebDriverManager.edgedriver().setup();
+		 WebDriver driver = new EdgeDriver();
+		 
+		 driver.get("https://www.google.com");
+		 driver.findElement(By.name("q")).sendKeys("Hello jenkins");
+		 Assert.fail();
+		 
+	}
+
+}
