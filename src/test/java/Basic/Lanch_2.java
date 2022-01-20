178b14af3dad5b0b9dd480c7f12d769cbe6506b2
Chethan K M <Chethan.km@Triconinfotech.com>
2022-01-20 16:59:11 +0530

    Second Commit...

--- src/test/java/Basic/Lanch_2.java
+++ src/test/java/Basic/Lanch_2.java
@@ -3,7 +3,9 @@ package Basic;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -16,6 +18,8 @@ public class Lanch_2 {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://www.google.com");
-		driver.findElement(By.name("q")).sendKeys("selenium");
+		WebElement search_bar = driver.findElement(By.name("q"));
+		search_bar.sendKeys("Darshan thoogudeepa");
+		search_bar.sendKeys(Keys.ENTER);
 	}
 }
