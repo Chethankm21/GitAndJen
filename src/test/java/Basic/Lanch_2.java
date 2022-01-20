9ee07f2e4111dd3601b04e6e073b0ebfd48b57ce
Chethan K M <Chethan.km@Triconinfotech.com>
2022-01-20 16:06:42 +0530

    initial commit...

---
+++ src/test/java/Basic/Lanch_2.java
@@ -0,0 +1,21 @@
+package Basic;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Lanch_2 {
+	public void lanch() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("selenium");
+	}
+}
---
+++ src/test/java/Basic/LanchingBrowser.java
@@ -0,0 +1,25 @@
+package Basic;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LanchingBrowser {
+
+	
+	@Test
+	public void lanch() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("selenium");
+	}
+}
